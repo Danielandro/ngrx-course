61d54899db7ecf0aa6d34cea58ee88f980e4891e
Danny <mtme13@hotmail.com>
2020-02-20 15:19:44 +0000

    Add FeatureSelector for AuthState

--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,11 +1,17 @@
-import { createSelector } from "@ngrx/store";
+import { createSelector, createFeatureSelector } from "@ngrx/store";
+import { AuthState } from "./reducers";
+
+// gets single feature state branch from global state
+// also adds type safety and autocomplete features
+export const selectAuthState = createFeatureSelector<AuthState>("auth");
 
 export const isLoggedIn = createSelector(
-  state => state["auth"], // slice of state needed - mapping function
+  // state => state["auth"] -> slice of state needed - mapping function
+  selectAuthState, // same as above line
   auth => !!auth.user // projector function
 );
 
 export const isLoggedOut = createSelector(
-  isLoggedIn,
+  isLoggedIn, // selectors are also mapping function so can be passed to other selectors
   loggedIn => !loggedIn
 );
