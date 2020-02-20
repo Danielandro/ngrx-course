b092c395564d6460f9abd728f183948b9394b4b7
Danny <mtme13@hotmail.com>
2020-02-20 13:09:05 +0000

    Show login/logout button depending on state * simple Store querying using observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
-import {Observable} from "rxjs";
-import {map} from 'rxjs/operators';
-import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map, tap } from 'rxjs/operators';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { AppState } from './reducers';
 
 @Component({
   selector: 'app-root',
@@ -11,37 +12,53 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 })
 export class AppComponent implements OnInit {
 
-    loading = true;
+  loading = true;
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
-    constructor(private router: Router) {
+  constructor(private router: Router, private store: Store<AppState>) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
+    // show whole state tree
+    // this.store.subscribe(state => console.log("Store value: ", state));
 
-      this.router.events.subscribe(event  => {
-        switch (true) {
-          case event instanceof NavigationStart: {
-            this.loading = true;
-            break;
-          }
+    // if user undefined, then return true
+    this.isLoggedOut$ = this.store.pipe(
+      tap(state => console.log("Current User: ", state["auth"].user)),
+      map(state => !state["auth"].user)
+    );
 
-          case event instanceof NavigationEnd:
-          case event instanceof NavigationCancel:
-          case event instanceof NavigationError: {
-            this.loading = false;
-            break;
-          }
-          default: {
-            break;
-          }
+    // if user undefined, return false
+    this.isLoggedIn$ = this.store.pipe(
+      tap(state => console.log("Current User: ", state["auth"].user)),
+      map(state => !!state["auth"].user) // DOUBLE BANG!!
+    );
+
+    this.router.events.subscribe(event => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
         }
-      });
 
-    }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
 
-    logout() {
+  }
 
-    }
+  logout() {
+
+  }
 
 }
