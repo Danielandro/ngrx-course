ff80c3532519015527606940bef505e93a342d0c
Danny <mtme13@hotmail.com>
2020-02-21 10:10:03 +0000

    Refacter login Effect * use CreateEffect * use ofType

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,17 +1,29 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { AuthActions } from "./action-types";
+import { tap } from "rxjs/operators";
 
 @Injectable()
 
 export class AuthEffects {
+  // createEffect will:
+  // * subscribe to the observable passed in to it
+  // * handle errors - observable gets re-created if error occurs
+  // * ofType allows Type safety
+  login$ = createEffect(() => {
+    return this.action$.pipe(
+      // filter for only login action
+      ofType(AuthActions.login),
+      // side effect here
+      tap(action => localStorage.setItem("user", JSON.stringify(action.user)))
+    );
+  },
+    { dispatch: false }); // this side effect does not result in dispatching of an action
+
   // Actions is an effects service that emits an observable when an action occurs
   constructor(private action$: Actions) {
-    // By subscribing, we have access to any actions emitted in this module
-    action$.subscribe(action => {
-      if (action.type === "[Login Page] User Login") {
-        localStorage.setItem("user", JSON.stringify(action.user));
-      }
-    });
+    // Actions is an observable stream of all Actions that are dispatched
+
   }
 
 
