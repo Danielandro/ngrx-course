34f51ac2870d4f0f2a50b19490d6b2fb92b6ceeb
Danny <mtme13@hotmail.com>
2020-02-24 16:34:38 +0000

    Use entities to define courses state * EntityState to define state interface * Entity adapter to access entity API * Save courses data to state

---
+++ src/app/courses/reducers/courses.reducers.ts
@@ -0,0 +1,22 @@
+import { EntityStateAdapter, EntityState } from "@ngrx/entity/src/models";
+import { Course } from "../model/course";
+import { createEntityAdapter } from "@ngrx/entity";
+import { createReducer, on } from "@ngrx/store";
+import { CourseActions } from "../action-types";
+
+// by extending the adapter, we now have access to the adapters CRUD methods
+// on our CoursesState
+export interface CoursesState extends EntityState<Course> {
+}
+
+// adapter exposes CRUD (+other) methods on entity api
+export const adapter = createEntityAdapter<Course>();
+
+// initial state: { ids: [], entities: {} }
+export const initialCoursesState = adapter.getInitialState();
+
+export const coursesReducer = createReducer(
+  initialCoursesState,
+  on(CourseActions.allCoursesLoaded, (state, action) => adapter.addAll(action.courses, state))
+);
+
